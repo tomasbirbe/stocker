6a72e820b52f884f978c7ba09a0c953bdb687214
Tomas Birbe <tbirbe@cibic.com.ar>
2023-01-27 14:00:18 -0300

    style(config): Add tailwind and it's config and css reset

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        backfill: { dark: "#242424" },
+        "text-color": { dark: "#C0C0C0" },
+      },
+    },
+  },
+  plugins: [],
+};
