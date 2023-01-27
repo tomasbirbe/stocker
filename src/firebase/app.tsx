edde7ff1260502998d2a3503cd3cd8775dceca98
Tomas Birbe <tomas.birbe@gmail.com>
2023-01-27 18:04:05 -0300

    feat(app): add firebase

---
+++ src/firebase/app.tsx
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC3rLXwajaGrvqB-PpVYKWuXlUMoaBx-uY",
+  authDomain: "stocker-9280e.firebaseapp.com",
+  projectId: "stocker-9280e",
+  storageBucket: "stocker-9280e.appspot.com",
+  messagingSenderId: "516763279323",
+  appId: "1:516763279323:web:e501c10b300011bfa5d1fe",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth();
