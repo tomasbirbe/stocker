011c4251d6a5ab8c69af827c0e7ff14747790609
Tomas Birbe <tomas.birbe@gmail.com>
2023-01-27 18:04:33 -0300

    feat(/login): add login page

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -4,8 +4,8 @@ type params = { label?: string } & InputHTMLAttributes<HTMLInputElement>;
 
 export const Input = ({ label, ...props }: params) => {
   return (
-    <label className="w-full" htmlFor={props.id}>
-      <p className="text-[14px] mb-2">{label}</p>
+    <label className="w-full flex flex-col gap-[7px]" htmlFor={props.id}>
+      <p className="text-[14px]">{label}</p>
       <input
         className="dark:bg-backfill-dark-400 w-full rounded-[4px] h-[40px] px-2 focus-visible:outline focus-visible:outline-1 focus-visible:outline-backfill-dark-200"
         type="text"
