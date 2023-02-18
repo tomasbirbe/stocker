5d291256e99d18f1d151bb9fd97cadd8b019c34d
Tomas Birbe <tomas.birbe@gmail.com>
2023-02-18 11:32:40 -0300

    style(Input): disable autocomplete for inputs

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -7,6 +7,7 @@ export const Input = ({ label, ...props }: params) => {
     <label className="w-full" htmlFor={props.id}>
       <p className="text-[14px] mb-2">{label}</p>
       <input
+        autoComplete="off"
         className="dark:bg-backfill-dark-400 w-full rounded-[4px] h-[40px] px-2 focus-visible:outline focus-visible:outline-1 focus-visible:outline-backfill-dark-200"
         type="text"
         {...props}
